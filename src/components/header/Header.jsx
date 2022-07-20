a3a12248c78896ba407899d49879503ecba1c2f5
wubsefer <wubsefermengistu@gmail.com>
2022-07-20 10:45:58 +0300

    search option designed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,15 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBed,
+  faCalendarDays,
   faCar,
   faMagnet,
+  faPerson,
   faPlane,
   faTaxi,
 } from "@fortawesome/free-solid-svg-icons";
 import "./Header.css";
+
 const Header = () => {
   return (
     <div className="header">
@@ -34,7 +37,23 @@ const Header = () => {
           </div>
         </div>
         <h1 className="headerTitle">Find your next stay</h1>
-        <p className="headerDisc">Search deals on hotels, homes, and much more...</p>
+        <p className="headerDisc">
+          Search deals on hotels, homes, and much more...
+        </p>
+        <div className="headerSearch">
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faBed} className="headerIcon" />
+            <input type="text" placeholder="Where you want to go" className="headerSearchInput" />
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
+            <span className="headerSearchText">Date to Date</span>
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faPerson} className="headerIcon" />
+            <span className="headerSearchText">2 Adult 2 Childern 1 room</span>
+          </div>
+        </div>
       </div>
     </div>
   );
